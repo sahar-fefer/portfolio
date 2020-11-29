8b5b90437eddd474eeccadd152f9c4269990628c
sahar feferovich <sahar.fefer@gmail.com>
2020-11-29 15:44:04 +0200

    litle fixes

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false)
-    }, 3000);
+    }, 700);
     return () => clearTimeout(timer);
   }, []);
 
