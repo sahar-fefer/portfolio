85ee8287d6b8313994586cd228728f1411268901
sahar feferovich <sahar.fefer@gmail.com>
2021-01-17 13:31:40 +0200

    update projects

--- src/components/website.jsx
+++ src/components/website.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import useHover from '../hooks/useHover';
 
-const Website = ({ name, title, description, techs, web, git }) => {
+const Website = ({ name, title, description, techs, web, gitFront, gitBack }) => {
     const [hoverRef, isHovered] = useHover();
 
     const [techLi, setTechLi] = useState([]);
@@ -22,9 +22,7 @@ const Website = ({ name, title, description, techs, web, git }) => {
     }, [])
 
     return (
-        // style={{backgroundImage: `url(/media/${title}.jpg)`}}
-        <div className={`${name} website`} ref={hoverRef}>
-            {/* {isHovered &&  */}
+        <div className={`${name} website`} ref={hoverRef} style={{backgroundImage: `url(${window.location.origin}/media/projects/${name}/1.PNG)`}}>
             <div className={`hover-website ${isHovered ? 'show' : 'hidden'}`}>
                 <h1 className="header perfect-center">{title}</h1>
                 <p className="description perfect-center">{description}</p>
@@ -34,15 +32,32 @@ const Website = ({ name, title, description, techs, web, git }) => {
                     </ul>
                 </div>
                 <div className="row justify-content-center">
-                    <a className="link-button col-auto" href={`${web ? web : '/'}`} target="_blank" >
-                        Vieu Sise {!web && ' - soon'}
-                    </a>
-                    <a className="link-button col-auto" href={`${git ? git : '/'}`} target="_blank" >
-                        GitHub  {!git && ' - soon'}
+                    {
+                        web
+                            ? <a className="link-button col-auto" href={web} target="_blank" >
+                                Vieu Sise
                             </a>
+                            : <div className="link-button col-auto" style={{ cursor: "context-menu" }}>
+                                Vieu Sise - soon
+                            </div>
+                    }
+                    {
+                        gitFront
+                            ? <a className="link-button col-auto" href={gitFront} target="_blank" >
+                                {gitBack && 'Website'} GitHub
+                            </a>
+                            : <div className="link-button col-auto" style={{ cursor: "context-menu" }}>
+                                GitHub - soon
+                                </div>
+                    }
+                    {
+                        gitBack &&
+                             <a className="link-button col-auto" href={gitBack} target="_blank" >
+                                Server GitHub
+                            </a>
+                    }
                 </div>
             </div>
-            {/* } */}
         </div>
     );
 };
