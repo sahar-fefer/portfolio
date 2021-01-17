76f147952c176bf524f165ae5d02794afe12bedc
sahar feferovich <sahar.fefer@gmail.com>
2021-01-17 18:21:24 +0200

    fix bugs and styling

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,40 +1,21 @@
-// import React from 'react';
-// import Typical from 'react-typical';
-
-// const Home = () => {
-//     return (
-//         <div id={'home'} className={'container-fluid'}>
-//             <div className={'header-wrapper'}>
-//                 <h1 className={'header'}>
-//                     Hey! I'm <span className={'name'}>Sahar Feferovich</span>
-//                 </h1>
-//                 <h2 className={'about'}>
-//                     I'm A Fullstack Developer
-//                 </h2>
-//                 <Typical
-//                     loop={Infinity}
-//                     className={'typing'}
-//                     steps={[
-//                         `text text`,
-//                         5000,
-//                         `text blabla`,
-//                         5000
-//                     ]}
-//                     wrapper="h2"
-//                 />
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Home;
-
-
 import React from 'react';
 import Typical from 'react-typical';
 
-const Home = ({ HOME }) => {
+const Home = ({ HOME, language }) => {
     const { TITLE, NAME, ABOUT, TYPING } = HOME;
+
+    const enTyping = [
+        "text text",
+        5000,
+        "text blabla",
+        5000
+    ]
+    const heTyping = [
+        "טקסט טקסט",
+        5000,
+        "טקסט בלהבלה",
+        5000
+    ]
 
     return (
         <div id={'home'} className={'container-fluid'}>
@@ -45,12 +26,24 @@ const Home = ({ HOME }) => {
                 <h2 className={'about'}>
                     {ABOUT}
                 </h2>
-                <Typical
-                    loop={Infinity}
-                    className={'typing'}
-                    steps={TYPING}
-                    wrapper="h2"
-                />
+                {
+                    language === 'en' &&
+                    <Typical
+                        loop={Infinity}
+                        className={'typing'}
+                        steps={enTyping}
+                        wrapper="h2"
+                    />
+                }
+                {
+                    language === 'he' &&
+                    <Typical
+                        loop={Infinity}
+                        className={'typing'}
+                        steps={heTyping}
+                        wrapper="h2"
+                    />
+                }
             </div>
         </div>
     );
