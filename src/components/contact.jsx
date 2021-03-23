5d79cba7eb2845e1a98dfdeb1c81bffbdae2ae34
saharovich <sahar.fefer@gmail.com>
2021-03-23 21:33:34 +0200

    fix transition open nav bar, fix contact header

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -9,7 +9,7 @@ import InputErrors from './inputErrors';
 const Contact = ({ CONTACT }) => {
     const { SECTION_TITLE, PARAGRAF1, PARAGRAF2, PARAGRAF3, PARAGRAF4, NAME, EMAIL, MESSAGE, SUBMIT, ERRORS } = CONTACT;
 
-    const [header, setHeadre] = useState(false);
+    const [header, setHeader] = useState(false);
     const [accordion1, setAccordion1] = useState(false);
     const [accordion2, setAccordion2] = useState(false);
     const [accordion3, setAccordion3] = useState(false);
@@ -47,7 +47,7 @@ const Contact = ({ CONTACT }) => {
         }
     })
 
-    const headrSwap = useSpring({
+    const headerSwap = useSpring({
         opacity: header ? 1 : 0,
         config: config.molasses,
         transform: header
@@ -95,13 +95,11 @@ const Contact = ({ CONTACT }) => {
         let isOK = true;
 
         for (const name in values) {
-            console.log(name)
             const field = values[name];
             const errors = validate(name, field.value, field.validations, ERRORS);
             if (errors.length) {
                 isOK = false;
                 setIsSubmmited('fillErr')
-                console.log('errors', errors)
                 setValues({
                     ...values,
                     [name]: {
@@ -115,10 +113,8 @@ const Contact = ({ CONTACT }) => {
         if (isOK) {
             emailjs.sendForm('service_56lr3eh', 'template_x4r5xea', e.target, "user_s4TwhJIxgZNXyXz3WcQwI")
                 .then((result) => {
-                    console.log('result', result);
                     setIsSubmmited(result && 'ok')
                 }, (error) => {
-                    console.log('error', error);
                     setIsSubmmited(error && 'submitErr')
                 });
             setValues({
@@ -143,8 +139,6 @@ const Contact = ({ CONTACT }) => {
         }
     }
 
-    console.log('values', values);
-
     return (
         <div id={'contact'}>
             <svg preserveAspectRatio="none" viewBox="0 0 100 102" height="75" width="100%"
@@ -152,154 +146,152 @@ const Contact = ({ CONTACT }) => {
                 <path d="M0 0 L50 100 L100 0 Z" fill="#e2e2e2" stroke="#e2e2e2"></path>
             </svg>
             <div className="container">
-                <div>
-                    <Waypoint
-                        bottomOffset="20%"
-                        topOffset='-20%'
-                        onEnter={() => {
-                            if (!header) setHeadre(true);
-                        }}
-                    />
-                    <animated.h1 style={headrSwap}
-                        className="component-header">
-                        {SECTION_TITLE}
-                    </animated.h1>
-                </div>
-                <div className="container">
-                    <div className={'wrapper row align-items-center'}>
-                        <div className={'col-md-6 col-lg ml-4 mr-4 ml-sm-0 mr-sm-0'}>
-                            <div>
-                                <Waypoint
-                                    bottomOffset="20%"
-                                    topOffset='-20%'
-                                    onEnter={() => {
-                                        if (!accordion1) setAccordion1(true);
-                                    }}
-                                />
-                                <animated.h3 style={{
-                                    transform: y1
-                                        .interpolate({
-                                            range: [0, 0.25, 0.5, 0.75, 1],
-                                            output: [0, 25, 50, 75, 100]
-                                        })
-                                        .interpolate(y1 => `translate3d(0, ${y1}px, 0)`),
-                                    marginBottom
-                                }}> {PARAGRAF1} </animated.h3>
-                            </div>
-                            <div>
-                                <Waypoint
-                                    bottomOffset="20%"
-                                    topOffset='-20%'
-                                    onEnter={() => {
-                                        if (!accordion2) setAccordion2(true);
-                                    }}
-                                />
-                                <animated.h3 style={{
-                                    transform: y2
-                                        .interpolate({
-                                            range: [0, 0.25, 0.5, 0.75, 1],
-                                            output: [0, 25, 50, 75, 100]
-                                        })
-                                        .interpolate(y2 => `translate3d(0, ${y2}px, 0)`),
-                                    marginBottom
-                                }}> {PARAGRAF2} </animated.h3>
-                            </div>
-                            <div>
-                                <Waypoint
-                                    bottomOffset="20%"
-                                    topOffset='-20%'
-                                    onEnter={() => {
-                                        if (!accordion3) setAccordion3(true);
-                                    }}
-                                />
-                                <animated.h3 style={{
-                                    transform: y3
-                                        .interpolate({
-                                            range: [0, 0.25, 0.5, 0.75, 1],
-                                            output: [0, 25, 50, 75, 100]
-                                        })
-                                        .interpolate(y3 => `translate3d(0, ${y3}px, 0)`),
-                                    marginBottom
-                                }}> {PARAGRAF3} </animated.h3>
-                            </div>
-                            <div>
-                                <Waypoint
-                                    bottomOffset="20%"
-                                    topOffset='-20%'
-                                    onEnter={() => {
-                                        if (!accordion4) setAccordion4(true);
-                                    }}
-                                />
-                                <animated.h3 style={{
-                                    transform: y4
-                                        .interpolate({
-                                            range: [0, 0.25, 0.5, 0.75, 1],
-                                            output: [0, 25, 50, 75, 100]
-                                        })
-                                        .interpolate(y4 => `translate3d(0, ${y4}px, 0)`),
-                                    marginBottom
-                                }}>
-                                    {PARAGRAF4}
-                                </animated.h3>
-                            </div>
+                <Waypoint
+                    bottomOffset="20%"
+                    topOffset='-20%'
+                    onEnter={() => {
+                        if (!header) setHeader(true);
+                    }}
+                />
+                <animated.h1 style={headerSwap}
+                    className="component-header">
+                    {SECTION_TITLE}
+                </animated.h1>
+            </div>
+            <div className="container">
+                <div className={'wrapper row align-items-center'}>
+                    <div className={'col-md-6 col-lg ml-4 mr-4 ml-sm-0 mr-sm-0'}>
+                        <div>
+                            <Waypoint
+                                bottomOffset="20%"
+                                topOffset='-20%'
+                                onEnter={() => {
+                                    if (!accordion1) setAccordion1(true);
+                                }}
+                            />
+                            <animated.h3 style={{
+                                transform: y1
+                                    .interpolate({
+                                        range: [0, 0.25, 0.5, 0.75, 1],
+                                        output: [0, 25, 50, 75, 100]
+                                    })
+                                    .interpolate(y1 => `translate3d(0, ${y1}px, 0)`),
+                                marginBottom
+                            }}> {PARAGRAF1} </animated.h3>
                         </div>
-                        <form className={'form col'} onSubmit={handleSubmit}>
-                            <input
-                                type="text"
-                                name={'name'}
-                                value={values['name'].value}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                placeholder={NAME} />
-                            {
-                                values['name'].errors &&
-                                <InputErrors errors={values['name'].errors}></InputErrors>
-                            }
-                            <input
-                                type="email"
-                                name={'email'}
-                                value={values['email'].value}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                placeholder={EMAIL} />
-                            {
-                                values.email.errors &&
-                                <InputErrors errors={values['email'].errors}></InputErrors>
-                            }
-                            <textarea
-                                type="text"
-                                name={'message'}
-                                value={values['message'].value}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                placeholder={MESSAGE}
-                                rows="6"
-                                cols="50" />
-                            {
-                                values['message'].errors &&
-                                <InputErrors errors={values['message'].errors}></InputErrors>
-                            }
-                            <input type="submit" value={SUBMIT} className={'submit'} />
-                            {
-                                isSubmmited === 'fillErr' &&
-                                <div>
-                                    {ERRORS['submit']['fillErr']}
-                                </div>
-                            }
-                            {
-                                isSubmmited === 'submitErr' &&
-                                <div>
-                                    {ERRORS['submit']['submitErr']}
-                                </div>
-                            }
-                            {
-                                isSubmmited === 'ok' &&
-                                <div>
-                                    {ERRORS['submit']['ok']}
-                                </div>
-                            }
-                        </form>
+                        <div>
+                            <Waypoint
+                                bottomOffset="20%"
+                                topOffset='-20%'
+                                onEnter={() => {
+                                    if (!accordion2) setAccordion2(true);
+                                }}
+                            />
+                            <animated.h3 style={{
+                                transform: y2
+                                    .interpolate({
+                                        range: [0, 0.25, 0.5, 0.75, 1],
+                                        output: [0, 25, 50, 75, 100]
+                                    })
+                                    .interpolate(y2 => `translate3d(0, ${y2}px, 0)`),
+                                marginBottom
+                            }}> {PARAGRAF2} </animated.h3>
+                        </div>
+                        <div>
+                            <Waypoint
+                                bottomOffset="20%"
+                                topOffset='-20%'
+                                onEnter={() => {
+                                    if (!accordion3) setAccordion3(true);
+                                }}
+                            />
+                            <animated.h3 style={{
+                                transform: y3
+                                    .interpolate({
+                                        range: [0, 0.25, 0.5, 0.75, 1],
+                                        output: [0, 25, 50, 75, 100]
+                                    })
+                                    .interpolate(y3 => `translate3d(0, ${y3}px, 0)`),
+                                marginBottom
+                            }}> {PARAGRAF3} </animated.h3>
+                        </div>
+                        <div>
+                            <Waypoint
+                                bottomOffset="20%"
+                                topOffset='-20%'
+                                onEnter={() => {
+                                    if (!accordion4) setAccordion4(true);
+                                }}
+                            />
+                            <animated.h3 style={{
+                                transform: y4
+                                    .interpolate({
+                                        range: [0, 0.25, 0.5, 0.75, 1],
+                                        output: [0, 25, 50, 75, 100]
+                                    })
+                                    .interpolate(y4 => `translate3d(0, ${y4}px, 0)`),
+                                marginBottom
+                            }}>
+                                {PARAGRAF4}
+                            </animated.h3>
+                        </div>
                     </div>
+                    <form className={'form col'} onSubmit={handleSubmit}>
+                        <input
+                            type="text"
+                            name={'name'}
+                            value={values['name'].value}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            placeholder={NAME} />
+                        {
+                            values['name'].errors &&
+                            <InputErrors errors={values['name'].errors}></InputErrors>
+                        }
+                        <input
+                            type="email"
+                            name={'email'}
+                            value={values['email'].value}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            placeholder={EMAIL} />
+                        {
+                            values.email.errors &&
+                            <InputErrors errors={values['email'].errors}></InputErrors>
+                        }
+                        <textarea
+                            type="text"
+                            name={'message'}
+                            value={values['message'].value}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            placeholder={MESSAGE}
+                            rows="6"
+                            cols="50" />
+                        {
+                            values['message'].errors &&
+                            <InputErrors errors={values['message'].errors}></InputErrors>
+                        }
+                        <input type="submit" value={SUBMIT} className={'submit'} />
+                        {
+                            isSubmmited === 'fillErr' &&
+                            <div>
+                                {ERRORS['submit']['fillErr']}
+                            </div>
+                        }
+                        {
+                            isSubmmited === 'submitErr' &&
+                            <div>
+                                {ERRORS['submit']['submitErr']}
+                            </div>
+                        }
+                        {
+                            isSubmmited === 'ok' &&
+                            <div>
+                                {ERRORS['submit']['ok']}
+                            </div>
+                        }
+                    </form>
                 </div>
             </div>
         </div>
